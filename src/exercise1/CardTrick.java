09322cd14eb95cf65ddf7554f5232740b816faa3
tonxiaol <134091784+tonxiaol@users.noreply.github.com>
2023-05-29 14:42:51 -0400

    Update CardTrick.java
    Modified author information

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -67,19 +67,17 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Xiaoling Tong");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Be a software engineer");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Reading");
+        System.out.println("-- Watching movies");
+        System.out.println("-- Playing tennis");
 
         System.out.println();
         
